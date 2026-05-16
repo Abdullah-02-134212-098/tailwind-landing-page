420da3712ac4d77a3a545d3373f36812b9e16dcf
Abdullah <muhammadabdullah.dev.098@gmail.com>
2026-05-16 17:25:01 +0500

    navbar update

--- src/components/Section1/Navbar.jsx
+++ src/components/Section1/Navbar.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-white flex justify-between items-center h-20">
-      <h4 className="bg-black text-white rounded-xl px-4 py-1 w-40 text-center ml-5">
+      <h4 className="bg-black text-white rounded-full px-4 py-1 w-40 text-center ml-5 text-sm tracking-widest">
         Target Audience
       </h4>
 
       
-      <button className="text-black font-mono uppercase mr-5 cursor-pointer">
+      <button className="text-black font-mono uppercase mr-5 cursor-pointer bg-gray-100  px-4 py-1 rounded-full text-sm tracking-widest">
         Digital Banking Platform
       </button>
     </div>
